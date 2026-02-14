1b8c60839a7c2abdd36be3ada9d63afb4d7d317e
TioElvis <elvis.veramijares@gmail.com>
2026-02-14 11:50:36 +0100

    Initialize NestJS application with health check endpoint and tests

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { AppController } from './app.controller';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('HealthCheck', () => {
+    it('Should return "OK"', () => {
+      expect(appController.HealthCheck()).toBe('OK');
+    });
+  });
+});
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  await app.listen(process.env.PORT ?? 9000);
+}
+
+void bootstrap();
