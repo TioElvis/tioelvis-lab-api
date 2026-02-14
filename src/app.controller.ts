ce37451dea1bf59dad586c4de58d11d0fe8eaa8f
TioElvis <elvis.veramijares@gmail.com>
2026-02-14 12:13:29 +0100

    Add redirect from root to health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 
 @Controller('/')
 export class AppController {
+  @Get('/')
+  @Redirect('/health-check', 302)
+  RedirectToHealthCheck() {}
+
   @Get('/health-check')
   HealthCheck() {
     return 'OK';
