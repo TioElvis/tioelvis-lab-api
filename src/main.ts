e178c2e633eab7a44e8e8cddf5fcb11a1cdf43d1
TioElvis <elvis.veramijares@gmail.com>
2026-02-14 12:05:30 +0100

    Add CORS configuration to enable specified HTTP methods

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,16 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
+const methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'];
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  // CONFIG
+  app.enableCors({ methods });
 
   await app.listen(process.env.PORT ?? 9000);
 }
