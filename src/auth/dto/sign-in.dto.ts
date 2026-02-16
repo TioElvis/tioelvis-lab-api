e901338a0d2409ca562d3f19d5d3213da4b0c9ff
TioElvis <elvis.veramijares@gmail.com>
2026-02-16 21:29:15 +0100

    Add validation rules to SignInDto: enforce non-empty email and password fields

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,9 +1,11 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignInDto {
+  @IsNotEmpty()
   @IsEmail({ host_whitelist: ['gmail.com'] })
   email: string;
 
+  @IsNotEmpty()
   @IsString()
   password: string;
 }
